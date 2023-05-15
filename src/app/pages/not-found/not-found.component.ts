874d42efb17ff018132928e4077312c10cd4a980
Maksat <maksat.madeniyetov@gmail.com>
2023-05-15 19:10:35 +0600

    feat: not-found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,8 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  template: `<div class="center center-y not-found">
+  <p class="not-found-text">No such page</p>
+  <a routerLink="/">Go home</a>
+</div>`,
+  styles: [
+    `.not-found {
+      height: 80vh;
+      background-color: black;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .not-found-text {
+      font-size: 50px;
+      color: white;
+    }
+
+    a {
+      margin-top: 20px;
+      font-size: 30px;
+      color: crimson;
+    }`]
 })
 export class NotFoundComponent {
 
