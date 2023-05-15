421cd8f60d0ca9499c67810ca85debb6efd3a664
Maksat <maksat.madeniyetov@gmail.com>
2023-05-15 20:29:39 +0600

    feat: each movie opens

--- src/app/components/UI/cards/movie-card/movie-card.component.ts
+++ src/app/components/UI/cards/movie-card/movie-card.component.ts
@@ -1,11 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input, OnDestroy} from '@angular/core';
 import {Movie} from "../../../../models/Movie";
+import {Subscription} from "rxjs";
+import {MovieService} from "../../../../services/movie.service";
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnDestroy {
   @Input() movie!: Movie;
+  subscriptions = new Subscription();
+  movieService = inject(MovieService);
+
+  setMovie() {
+    this.subscriptions.add(
+      this.movieService.getMovieById(this.movie.id).subscribe(data => {
+        this.movieService.movie.next(data);
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
