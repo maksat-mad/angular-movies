2f100b18a19277ef22d5327ebf2a62a0a1ec1e5e
Maksat <maksat.madeniyetov@gmail.com>
2023-05-16 21:32:50 +0600

    feat: my favorites functionailty

--- src/app/pages/favorite-movies/favorite-movies.component.ts
+++ src/app/pages/favorite-movies/favorite-movies.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MovieService} from "../../services/movie.service";
 
 @Component({
   selector: 'app-favorite-movies',
   templateUrl: './favorite-movies.component.html'
 })
 export class FavoriteMoviesComponent {
-
+  movieService = inject(MovieService);
 }
