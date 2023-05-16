464603fd0e78632fb25410def2c99ad29c3bc19b
Maksat <maksat.madeniyetov@gmail.com>
2023-05-17 00:54:40 +0600

    chore: default movie change

--- src/app/services/defaultMovie.ts
+++ src/app/services/defaultMovie.ts
@@ -1,43 +1,47 @@
-export const defaultMovie:any = {
+export const defaultMovie: any = {
   "adult": false,
-  "backdrop_path": "/jHxCeXnSchAuwHnmVatTgqMYdX8.jpg",
+  "backdrop_path": "/8rpDcsfLJypbO6vREc0547VKqEv.jpg",
   "belongs_to_collection": {
-    "id": 556,
-    "name": "Spider-Man Collection",
-    "poster_path": "/3BePoRxiaWcfFcUdZi4bveQJN97.jpg",
-    "backdrop_path": "/waZqriYTuBE3WqXI3SDGi3kfDQE.jpg"
+    "id": 87096,
+    "name": "Avatar Collection",
+    "poster_path": "/wO67q3lInOFKplkw9wyzrWcvVoM.jpg",
+    "backdrop_path": "/iaEsDbQPE45hQU2EGiNjXD2KWuF.jpg"
   },
-  "budget": 139000000,
+  "budget": 460000000,
   "genres": [
     {
-      "id": 14,
-      "name": "Fantasy"
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
     },
     {
       "id": 28,
       "name": "Action"
     }
   ],
-  "homepage": "https://www.sonypictures.com/movies/spiderman",
-  "id": 557,
-  "imdb_id": "tt0145487",
+  "homepage": "https://www.avatar.com/movies/avatar-the-way-of-water",
+  "id": 76600,
+  "imdb_id": "tt1630029",
   "original_language": "en",
-  "original_title": "Spider-Man",
-  "overview": "After being bitten by a genetically altered spider at Oscorp, nerdy but endearing high school student Peter Parker is endowed with amazing powers to become the superhero known as Spider-Man.",
-  "popularity": 79.907,
-  "poster_path": "/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg",
+  "original_title": "Avatar: The Way of Water",
+  "overview": "Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+  "popularity": 1945.368,
+  "poster_path": "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
   "production_companies": [
     {
-      "id": 19551,
-      "logo_path": "/2WpWp9b108hizjHKdA107hFmvQ5.png",
-      "name": "Marvel Enterprises",
+      "id": 127928,
+      "logo_path": "/h0rjX5vjW5r8yEnUBStFarjcLT4.png",
+      "name": "20th Century Studios",
       "origin_country": "US"
     },
     {
-      "id": 326,
-      "logo_path": null,
-      "name": "Laura Ziskin Productions",
-      "origin_country": ""
+      "id": 574,
+      "logo_path": "/iB6GjNVHs5hOqcEYt2rcjBqIjki.png",
+      "name": "Lightstorm Entertainment",
+      "origin_country": "US"
     }
   ],
   "production_countries": [
@@ -46,9 +50,9 @@ export const defaultMovie:any = {
       "name": "United States of America"
     }
   ],
-  "release_date": "2002-05-01",
-  "revenue": 821708551,
-  "runtime": 121,
+  "release_date": "2022-12-14",
+  "revenue": 2319794231,
+  "runtime": 192,
   "spoken_languages": [
     {
       "english_name": "English",
@@ -57,9 +61,9 @@ export const defaultMovie:any = {
     }
   ],
   "status": "Released",
-  "tagline": "With great power comes great responsibility.",
-  "title": "Spider-Man",
+  "tagline": "Return to Pandora.",
+  "title": "Avatar: The Way of Water",
   "video": false,
-  "vote_average": 7.267,
-  "vote_count": 17083
+  "vote_average": 7.706,
+  "vote_count": 7989
 }
